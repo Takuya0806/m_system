2e0cffd40feec31f118736f13fb74fdafdb6e137
Takuya0806 <tiphereth8192@gmail.com>
2019-03-21 18:33:58 +0900

    Fixed database_config.php

--- database_config.php
+++ database_config.php
@@ -2,10 +2,10 @@
 
    define("DB_SERVER",'shared-dbinstance-new.cu37i3qxmw8t.ap-northeast-1.rds.amazonaws.com');
 
-   define("DB_ACCOUNT_ID",'t_katsumata');
+   define("DB_ACCOUNT_ID",'t_xxxxxxxxx');
 
-   define("DB_ACCOUNT_PW",'Katsumata_201709');
+   define("DB_ACCOUNT_PW",'Kxxxxxxxx_201709');
 
-   define("DB_NAME",'db_t_katsumata');
+   define("DB_NAME",'db_t_xxxxxxxxx');
 
 ?>
